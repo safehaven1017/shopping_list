9c98e818e0a409b834f0fedc9d256bb05fb164c4
Rick Rackley <93223405+safehaven1017@users.noreply.github.com>
2021-11-16 18:27:13 -0500

    create main skeleton

--- main.py
+++ main.py
@@ -6,4 +6,33 @@ def main():
     clear = lambda: os.system('clear')
 
     while True:
+        print("SHOPPING HELPER")
+        print("---------------\n")
+        print("1. CREATE SHOPPING LISTS")
+        print("2. DELETE SHOPPING LISTS")
+        print("3. VIEW SHOPPING LISTS")
         
+        # get user input
+        user_input = validate_menu_selection(1, 5)
+
+        # create shopping lists
+        if user_input == 1:
+            clear()
+            create_lists(shopping_lists)
+
+        # delete shopping lists
+        elif user_input == 2:
+            clear()
+            delete_lists(shopping_lists)
+        
+        # view shopping lists
+        elif user_input == 3:
+            clear()
+            view_lists(shopping_lists)
+        # quit
+        else:
+            clear()
+            quit()
+
+
+
